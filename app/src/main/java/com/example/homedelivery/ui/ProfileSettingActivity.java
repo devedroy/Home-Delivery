3a8c53a60d1c3e8e3262437f7c9dcdadf52d6901
Roy <devedroy.dr@gmail.com>
2022-07-14 18:10:10 +0530

    Day2 - UI

---
+++ app/src/main/java/com/example/homedelivery/ui/ProfileSettingActivity.java
@@ -0,0 +1,16 @@
+package com.example.homedelivery.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.homedelivery.R;
+
+public class ProfileSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_setting);
+    }
+}
