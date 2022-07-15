e9a223a5c7b68e7e64795b6a85246da1e2f0b939
Roy <devedroy.dr@gmail.com>
2022-07-15 19:00:58 +0530

    Day3 - UI

---
+++ app/src/main/java/com/example/homedelivery/ui/LOGIN.java
@@ -0,0 +1,16 @@
+package com.example.homedelivery.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.homedelivery.R;
+
+public class LOGIN extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
